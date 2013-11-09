ab3d5f1b04b4a6de4c22d1202fad104626fc319c
unknown <bob.craker@hotmail.com>
2013-11-09 14:18:53 -0800

    added momentum component and ball texture

---
+++ Broach/Broach/Broach/Framework/Components/MomentumComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Broach
+{
+    class MomentumComponent : GameComponent
+    {
+        private PositionComponent positionComponent;
+        private Vector2 velocity;
+
+        public PositionComponent PositionComponent
+        {
+            get { return positionComponent; }
+            set { positionComponent = value; }
+        }
+
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+
+        public MomentumComponent(PositionComponent positionComponent, Vector2 velocity)
+        {
+            this.positionComponent = positionComponent;
+            this.velocity = velocity;
+
+            Game1.Systems["Momentum"].Components.Add(this);
+        }
+    }
+}
---
+++ Broach/Broach/Broach/Framework/Systems/MomentumSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Broach
+{
+    class MomentumSystem : GameSystem
+    {
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            foreach (MomentumComponent momentumComponent in Components)
+            {
+                Vector2 myPosition = ((MomentumComponent)momentumComponent).PositionComponent.Position;
+                ((MomentumComponent)momentumComponent).PositionComponent.Position = new Vector2(myPosition.X + momentumComponent.Velocity.X, myPosition.Y + momentumComponent.Velocity.Y);
+            }
+        }
+    }
+}
